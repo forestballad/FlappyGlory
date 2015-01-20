ddbee705d74badfdc38e85a2c2a5feb4c173840b
Anli Ji <anli.azuresand@gmail.com>
2015-01-19 23:18:28 -0500

    First Level & Some Arts

---
+++ Assets/Scripts/EnterLevel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterLevel : MonoBehaviour {
+	public string LevelName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartLevel(){
+		Application.LoadLevel ("play_original");
+	}
+}
---
+++ Assets/Scripts/PlayerBird.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBird : MonoBehaviour {
+	public Vector2 jumpForce = new Vector2(0,300);
+	public Sprite BirdUp;
+	public Sprite BirdDown;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp ("space")) {
+			rigidbody2D.velocity = Vector2.zero;
+			rigidbody2D.AddForce(jumpForce);
+		}
+		if (rigidbody2D.velocity.y >= 0) {
+			GetComponent<SpriteRenderer>().sprite = BirdUp;
+		}
+		else{
+			GetComponent<SpriteRenderer>().sprite = BirdDown;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		Die();
+	}
+
+	void Die(){
+		Application.LoadLevel("title");
+	}
+}

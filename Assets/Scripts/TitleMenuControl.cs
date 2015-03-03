67db84fe5e21f0ee590bb7c2e50a72249a4fe91d
Anli Ji <anli.azuresand@gmail.com>
2015-03-03 01:08:25 -0500

    Guess what?
    Real Art!

---
+++ Assets/Scripts/TitleMenuControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleMenuControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void EnterGameMenu(){
+		Application.LoadLevel ("menu_game");
+	}
+
+	public void EnterHighScore(){
+	}
+}

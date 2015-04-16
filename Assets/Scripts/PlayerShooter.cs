e4c206f1fb9375c8071f825a4a8b749c5af00e70
Anli Ji <anli.azuresand@gmail.com>
2015-04-16 02:21:57 -0400

    Create the STG level

---
+++ Assets/Scripts/PlayerShooter.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooter : MonoBehaviour {
+	public float NormalSpeed = 0f;
+	public float ShiftSpeed = 0f;
+	public float BoundingBoxUp = 0f;
+	public float BoundingBoxDown = 0f;
+	public float BoundingBoxLeft = 0f;
+	public float BoundingBoxRight = 0f;
+	public Sprite NormalSprite;
+	public Sprite ShiftSprite;
+	bool shiftStance = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift))
+			shiftStance = true;
+		else 
+			shiftStance = false;
+		if (shiftStance) {
+			gameObject.GetComponent<SpriteRenderer> ().sprite = ShiftSprite;
+		} else
+		{
+			gameObject.GetComponent<SpriteRenderer> ().sprite = NormalSprite;
+		}
+	}
+
+	void FixedUpdate(){
+		float movex = Input.GetAxis ("Horizontal");
+		float movey = Input.GetAxis ("Vertical");
+		float targetX;
+		float targetY;
+		if (shiftStance) {
+			targetX= gameObject.transform.position.x + movex * ShiftSpeed;
+			targetY = gameObject.transform.position.y + movey * ShiftSpeed;
+		}
+		else{
+			targetX= gameObject.transform.position.x + movex * NormalSpeed;
+			targetY = gameObject.transform.position.y + movey * NormalSpeed;
+		}
+		if (targetX > BoundingBoxRight)	targetX = BoundingBoxRight;
+		if (targetX < BoundingBoxLeft)	targetX = BoundingBoxLeft;
+		if (targetY > BoundingBoxUp)	targetY = BoundingBoxUp;
+		if (targetY < BoundingBoxDown)	targetY = BoundingBoxDown;
+		gameObject.transform.position = new Vector2 (targetX, targetY);
+	}
+}

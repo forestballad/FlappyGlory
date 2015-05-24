d224dcade57351493f387cc34e37c83f3cfdbf09
Anli Ji <anli.azuresand@gmail.com>
2015-05-24 12:26:31 -0400

    DaMoGuYan game part

---
+++ Assets/HitPillarFieldDetection.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitPillarFieldDetection : MonoBehaviour {
+	public PlayerBreaker scriptRef;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		scriptRef.inPosition = true;
+	}
+}
---
+++ Assets/PlayerBreaker.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBreaker : MonoBehaviour {
+	public float speed;
+	public bool inPosition;
+
+	int hitTime;
+
+	// Use this for initialization
+	void Start () {
+		inPosition = false;
+		hitTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("right")) {
+			gameObject.transform.position = new Vector2(gameObject.transform.position.x + speed,gameObject.transform.position.y);
+		}
+		if (Input.GetKeyDown (KeyCode.Space) && inPosition) {
+			hitTime ++;
+		}
+		if (hitTime == 20) {
+			GameObject.Find ("DataAgentObject").GetComponent<DataAgent> ().UnlockCharacter (13);
+			Application.LoadLevel ("game");
+		} 
+	}
+}
